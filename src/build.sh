6edeebeb5ec220b85c8c05b61b257974ad8666a8
Jephte Clain <Jephte.Clain@univ-reunion.fr>
2025-11-16 13:13:34 +0400

    importation initiale

---
+++ src/build.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# -*- coding: utf-8 mode: sh -*- vim:sw=4:sts=4:et:ai:si:sta:fenc=utf-8
+source /src/moodle.conf || exit 1
+
+MYDIR="$(cd "$(dirname "$0")"; pwd)"
+MYNAME="$(basename "$0")"
+
+function die() {
+    echo "ERREUR FATALE: $*" 1>&2
+    exit 1
+}
+
+# installer les outils nécessaires
+apt-get update && apt-get install -y "${packages[@]}"
+
+# configurer php
+eval "inis=($(cd /src/php; ls *.ini 2>/dev/null))"
+for ini in "${inis[@]}"; do
+    ln -s "/src/php/$ini" "/usr/local/etc/php/conf.d/$ini"
+done
+ln -sf /usr/local/etc/php/php.ini-development /usr/local/etc/php/php.ini
+
+for phpext in "${phpexts[@]}"; do
+    configure="${phpext}_configure[@]"; configure="${!configure}"
+    echo "\
+================================================================================
+== $phpext ${configure[*]}"
+    docker-php-ext-configure "$phpext" "${configure[@]}"
+    docker-php-ext-install "$phpext"
+done
+
+# installer composer
+ln -sf /src/composer/composer.phar /usr/local/bin/composer
+
+# configurer apache
+ln -sf /src/apache/000-default.conf /etc/apache2/sites-enabled/000-default.conf
+ln -sf /src/apache/ports.conf /etc/apache2/ports.conf
+
+# créer l'utilisateur moodle
+useradd -m -u 1000 -U -d /var/www/moodle moodle
+
+# installer moosh
+eval "files=($(ls /src/install/moosh_*.zip 2>/dev/null))"
+[ ${#files[*]} -gt 0 ] || die "Impossible de trouver l'archive moosh"
+
+unzip "${files[0]}" -d /opt
+chown moodle: /opt/moosh
+ln -s /opt/moosh/moosh.php /usr/local/bin/moosh
+
+# répertoire de données
+mkdir -p /var/www/moodledata
+chown moodle: /var/www/moodledata
+
+# installer moodle
+eval "files=($(ls /src/install/moodle-*.tgz 2>/dev/null))"
+[ ${#files[*]} -gt 0 ] || die "Impossible de trouver l'archive moodle"
+
+tar xzf "${files[0]}" -C /var/www
+chown -R moodle: /var/www/moodle
+
+exit 0
---
+++ src/entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# -*- coding: utf-8 mode: sh -*- vim:sw=4:sts=4:et:ai:si:sta:fenc=utf-8
+source /src/moodle.conf || exit 1
+
+MYDIR="$(cd "$(dirname "$0")"; pwd)"
+MYNAME="$(basename "$0")"
+
+function die() {
+    echo "ERREUR FATALE: $*" 1>&2
+    exit 1
+}
+
+# attendre que mysql est disponible
+echo "# attente de mysql"
+while true; do
+    if mysql -hdb -umoodle -pmoodle -e "select 1" >&/dev/null; then
+        break
+    fi
+    echo "... attente de mysql"
+    sleep 1
+done
+
+# configurer moodle
+cd /var/www/moodle
+if [ ! -d vendor ]; then
+    composer i
+fi
+if [ ! -f config.php ]; then
+    echo "# installation de moodle"
+    cd admin/cli
+    php install.php "${initial_config[@]}"
+
+    echo "# configuration initiale de moodle"
+    /src/moodle.init
+fi
+
+# démarrer apache
+echo "# démarrage apache"
+cd /var/www/moodle
+export APACHE_RUN_USER=moodle
+export APACHE_RUN_GROUP=moodle
+exec /usr/local/bin/apache2-foreground
